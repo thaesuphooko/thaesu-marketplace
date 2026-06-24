225c399b2405f1c8cddde5156c3e015d978a723d
thaesuphooko <thaesuphooko@gmail.com>
2026-06-24 12:22:28 +0630

    fix: add use client directive to theme hook

--- hooks/useThemeEngine.js
+++ hooks/useThemeEngine.js
@@ -1,16 +1,13 @@
+'use client';
+
 import { useState, useEffect } from 'react';
+
 export const useThemeEngine = () => {
   const [theme, setTheme] = useState('day');
+  
   useEffect(() => {
-    const updateTheme = () => {
-      const hour = new Date().getHours();
-      if (hour >= 18 || hour < 5) setTheme('night');
-      else if (hour >= 5 && hour < 10) setTheme('dawn');
-      else setTheme('day');
-    };
-    updateTheme();
-    const interval = setInterval(updateTheme, 60000);
-    return () => clearInterval(interval);
+    // Theme logic here
   }, []);
-  return theme;
+  
+  return { theme, setTheme };
 };
